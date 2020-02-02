f09a00b0b81effe350a320e72eb6d12ffd77d45a
Langster2323 <langahkeem@gmail.com>
2020-02-02 08:01:03 -0600

    Keeping separate

--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Todos = ({ todos }) => {
-    return (
+const Todos = ({ todos }) => { 
+
+    const todoList = todos.length ? (
         <div className="todos collection">
             {todos.map(todo => {
                 return <div key={todo.id}>{todo.content}</div>
             })}
+        </div>
+    ) : <p>No todos!</p>
+    return (
+        <div>
+            {todoList}
         </div>
     )
 }
