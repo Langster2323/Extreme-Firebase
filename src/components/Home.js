5094fcf37e3d767396cc47a36cde4ef0548426f3
Langster2323 <langahkeem@gmail.com>
2020-02-05 10:04:18 -0600

    Connected components to redux store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            console.log(res);
-            this.setState({
-                posts: res.data.slice(0, 10)
-            })
-        })
-    }
-
     render() {
-        const { posts } = this.state;
+        console.log(this.props);    
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -48,7 +31,12 @@ class Home extends Component {
             </div>
         )
     }
-    
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'asd saldkjf asdlfkjas sdlfkj', body: 'saldkjfasl sadk asdfsadf lkue  vlksdjir sakel seerl aslalkjerga keufgglawoqpv uuipvzmn'},
+        {id: '2', title: 'asd saldkjf asdlfkjas sdlfkj', body: 'saldkjfasl sadk asdfsadf lkue  vlksdjir sakel seerl aslalkjerga keufgglawoqpv uuipvzmn'},
+        {id: '3', title: 'asd saldkjf asdlfkjas sdlfkj', body: 'saldkjfasl sadk asdfsadf lkue  vlksdjir sakel seerl aslalkjerga keufgglawoqpv uuipvzmn'}
+    ]
 }
 
-const rootReducer = (state = initState, action) {
+const rootReducer = (state = initState, action) => {
     return state;
 }
 
